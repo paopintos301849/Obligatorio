aa7b87bcba39e6ec624938240f5ddd7dffd16fa0
paopintos301849 <pp301849@fi365.ort.edu.uy>
2025-04-03 22:23:40 -0300

    Menu de opciones

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -6,6 +6,97 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello world");
+        string opcion = "";
+        
+        while (opcion != "0") {
+            MostrarMenu();
+            Console.WriteLine("Ingrese una opción");
+            opcion = Console.ReadLine();
+            
+            switch (opcion) {
+                case "1":
+                    OpcionListarClientes();
+                    PresioneParaContinuar();
+                    break;
+                case "2":
+                    OpcionListarVuelosSegunAeropuerto();
+                    PresioneParaContinuar();
+                    break;
+                case "3":
+                    OpcionAltaClienteOcasional();
+                    PresioneParaContinuar();
+                    break;
+                case "4":
+                    OpcionListarPasajesEntreFechas();
+                    PresioneParaContinuar();
+                    break;
+                case "0":
+                    Console.WriteLine("Saliendo...");
+                    break;
+                default: 
+                    MostrarError("ERROR: Opción inválida");
+                    PresioneParaContinuar();
+                    break;
+            }
+        }
+    }
+
+    static void MostrarMenu() {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine("***** MENU *****");
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("1 - Listar todos los clientes");
+        Console.WriteLine("2 - Listar vuelos por código de aeropuerto");
+        Console.WriteLine("3 - Alta de cliente ocasional");
+        Console.WriteLine("4 - Listar pasajes entre fechas");
+        Console.WriteLine("0 - Salir");
+    }
+
+    /*Listado de todos los clientes. Mostrar nombre, email, nacionalidad y puntos o si es elegible, según
+    corresponda.*/
+    static void OpcionListarClientes()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("***** Listar todos los clientes *****");
+        Console.ForegroundColor = ConsoleColor.Gray;       
+    }
+    
+    /*Dado un código de aeropuerto listar todos los vuelos que lo incluyen. Mostrar número de vuelo,
+    modelo del avión, ruta (en formato MVD – MIA), y frecuencia.*/
+    static void OpcionListarVuelosSegunAeropuerto()
+    {
+        
+    }
+    
+    /*Alta de cliente ocasional.*/
+    static void OpcionAltaClienteOcasional()
+    {
+        
+    }
+    
+    /*Dadas dos fechas, listar los pasajes entre esas fechas. Mostrar id, nombre del pasajero, precio, fecha
+    y número de vuelo.*/
+    static void OpcionListarPasajesEntreFechas()
+    {
+        
+    }
+    
+    static void MostrarError(string mensaje) {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(mensaje);
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+    
+    static void MostrarExito(string mensaje) {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(mensaje);
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+
+    static void PresioneParaContinuar() {
+        Console.WriteLine("Presione una tecla para continuar...");
+        Console.ReadKey();
     }
 }
--- Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
+++ Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bbc4ca5a872e57781127082f4f7d39d7d9a65ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
